439899d59f4c2a681a860990aa8b550b58637aeb
Artem <kor.artemik@yandex.ru>
2022-05-29 17:16:37 +0300

    adding dto

---
+++ src/main/java/com/example/demo/dto/UserNameDto.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserNameDto {
+
+    @JsonProperty("user_name")
+    @Size(max=255)
+    @NotBlank
+    private String userName;
+}
